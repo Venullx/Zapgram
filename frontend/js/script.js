fe48a51026a28965ff348b037aed0f062853c60b
Venullx <emanoelgomes47ti@gmail.com>
2024-10-12 12:43:04 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -124,3 +124,16 @@ document.getElementById('themeToggle').addEventListener('click', function() {
         moonIcon.style.display = 'none';
     }
 });
+
+
+// Responsividade
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('navbarToggle');
+    const menu = document.getElementById('navbarMenu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('active'); // Alterna a classe 'active' no menu
+    });
+});
+
