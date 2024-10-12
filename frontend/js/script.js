2c807e7ef3bb4d1c62ab9c1a1dd5df8f3ebc1731
Venullx <emanoelgomes47ti@gmail.com>
2024-10-12 12:10:30 -0300

    Upload v1.0

---
+++ frontend/js/script.js
@@ -0,0 +1,126 @@
+// login elements
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
+
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
+
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
+]
+
+const user = { id: "", name: "", color: "" }
+
+let websocket
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message--other")
+
+    span.classList.add("message--sender")
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data)
+
+    const message =
+        userId == user.id
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor)
+
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
+
+const handleLogin = (event) => {
+    event.preventDefault()
+
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+}
+
+const sendMessage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message))
+
+    chatInput.value = ""
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
+
+// temas
+document.getElementById('themeToggle').addEventListener('click', function() {
+    // Alternar a classe 'dark-mode' no body para mudar o tema
+    document.body.classList.toggle('dark-mode');
+
+    // Alternar a visibilidade dos ícones de sol e lua
+    const sunIcon = document.querySelector('.sun');
+    const moonIcon = document.querySelector('.moon');
+
+    if (document.body.classList.contains('dark-mode')) {
+        // Quando o tema escuro está ativo, mostrar o ícone de lua e esconder o de sol
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'block';
+    } else {
+        // Quando o tema claro está ativo, mostrar o ícone de sol e esconder o de lua
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
+    }
+});
